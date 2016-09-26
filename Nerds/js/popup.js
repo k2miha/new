012a15f8f6ca0706bb67b14d69430dfcc48e2f48
k2miha <k2-miha@mail.ru>
2016-09-26 23:59:22 +0300

    Закончил скриптинг Нёрдса

--- Nerds/js/popup.js
+++ Nerds/js/popup.js
@@ -5,9 +5,11 @@ var form = popup.querySelector("form");
 var login = popup.querySelector("[name=name]");
 var password = popup.querySelector("[name=email]");
 var textarea = popup.querySelector("[name=letter]");
+var overlay = document.querySelector(".modal-overlay");
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("modal-content-show");
+  overlay.classList.add("overlay-show");
   var storage = localStorage.getItem("name");
   if(storage) {
     login.value = storage;
@@ -22,6 +24,7 @@ link.addEventListener("click", function(event) {
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("modal-content-show");
+  overlay.classList.remove("overlay-show");
   popup.classList.remove("modal-error");
 });
 form.addEventListener("submit", function(event) {
@@ -38,6 +41,7 @@ window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (popup.classList.contains ("modal-content-show")) {
       popup.classList.remove("modal-content-show");
+      overlay.classList.remove("overlay-show");
       popup.classList.remove("modal-error");
     }
   }
