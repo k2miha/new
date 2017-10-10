cb9b6f62f4216561d986f9a622eee9445dadacda
k2miha <k2-miha@mail.ru>
2017-10-10 23:24:22 +0300

    Закончил верстку статического состояния страниц

--- Pink/js/hamburger.js
+++ Pink/js/hamburger.js
@@ -1,6 +1,7 @@
 var menuOpenButton = document.querySelector(".logotype__menu-button");
 var menuCloseButton = document.querySelector(".menu__button");
 var menu = document.querySelector(".menu");
+var menuLink = document.querySelectorAll(".menu__elements-container .menu__link");
 menuOpenButton.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.add("menu--show");
@@ -8,4 +9,8 @@ menuOpenButton.addEventListener("click", function(event) {
 menuCloseButton.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.remove("menu--show");
+});
+menuLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  menuLink.classList.add("menu--active");
 });
--- Pink/js/modal.js
+++ Pink/js/modal.js
@@ -4,23 +4,27 @@ var modalCloseButton = document.querySelector(".pop-up_failure .pop-up__button")
 var modalCloseButtonSuccess = document.querySelector(".pop-up__button");
 var modalSuccess = document.querySelector(".pop-up");
 var modalFail = document.querySelector(".pop-up_failure");
+var overlay = document.querySelector(".overlay ");
 var form = document.querySelector("form");
-
 modalCloseButton.addEventListener("click", function(event) {
   event.preventDefault();
   modalFail.classList.remove("pop-up--show");
+  overlay.classList.remove("overlay--show");
 });
 modalCloseButtonSuccess.addEventListener("click", function(event) {
   event.preventDefault();
   modalSuccess.classList.remove("pop-up--show");
+  overlay.classList.remove("overlay--show");
 });
 form.addEventListener("submit", function(event) {
   if(login.value !="") {
     event.preventDefault();
     modalSuccess.classList.add("pop-up--show");
+    overlay.classList.add("overlay--show");
   }
   else {
     event.preventDefault();
     modalFail.classList.add("pop-up--show");
+    overlay.classList.add("overlay--show");
   }
 });
