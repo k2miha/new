870ede566a5b5332841b140e073e352c0b25ece6
k2miha <k2-miha@mail.ru>
2017-10-15 12:51:11 +0300

    Начал верстку стайлгайда

--- Pink/js/hamburger.js
+++ Pink/js/hamburger.js
@@ -1,7 +1,6 @@
 var menuOpenButton = document.querySelector(".logotype__menu-button");
 var menuCloseButton = document.querySelector(".menu__button");
 var menu = document.querySelector(".menu");
-var menuLink = document.querySelectorAll(".menu__elements-container .menu__link");
 menuOpenButton.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.add("menu--show");
@@ -9,8 +8,4 @@ menuOpenButton.addEventListener("click", function(event) {
 menuCloseButton.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.remove("menu--show");
-});
-menuLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  menuLink.classList.add("menu--active");
 });
