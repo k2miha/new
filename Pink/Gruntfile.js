4e1e4f9e5c718d8aa29e7b5f3e9231dcacc6b13d
k2miha <k2-miha@mail.ru>
2017-11-23 23:18:45 +0300

    Мелкие правки Барбершопа и Пинк

--- Pink/Gruntfile.js
+++ Pink/Gruntfile.js
@@ -1,5 +1,57 @@
 module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
   grunt.initConfig({
+    clean: {
+      build: ["build"]
+   },
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**",
+            "*.html"
+          ],
+           dest: "build"
+          }]
+        }
+    },
+    postcss: {
+      style: {
+      options: {
+        processors: [
+          require('precss')(),
+          require("autoprefixer")({browsers:
+             [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+              ]}),
+          require("css-mqpacker")({
+            sort: true
+          })
+        ]
+      },
+      dist: {
+        src: 'postcss/style.css',
+        dest: 'build/css/style.css'
+      }
+     }
+    },
+     csso: {
+          style: {
+                    options: {
+                        report: "gzip"
+                           },
+                           files: {
+                             "build/css/style.min.css": ["build/css/style.css"]
+                            }
+                          }
+                    },
     watch: {
                 css: {
                     files: ['postcss/**/*.css'],
@@ -10,19 +62,6 @@ module.exports = function(grunt) {
                     }
                 }
             },
-    postcss: {
-  		options: {
-  			processors: [
-  				require('precss')()
-  			]
-  		},
-  		dist: {
-        src: 'postcss/style.css',
-        dest: 'css/style.css'
-  		}
-  	},
-
-
     browserSync: {
       bsFiles: {
         src : ['css/*.css', '*.html', 'img/*.*', 'js/*.js']
