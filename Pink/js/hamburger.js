32fd700fedf9d0c53c27596b44f18473f9da9d15
Mike Kolotovich <k2-miha@mail.ru>
2017-12-26 23:35:30 +0300

    Поправил меню на мобильном и планшете

--- Pink/js/hamburger.js
+++ Pink/js/hamburger.js
@@ -1,11 +1,14 @@
 var menuOpenButton = document.querySelector(".logotype__menu-button");
 var menuCloseButton = document.querySelector(".menu__button");
 var menu = document.querySelector(".menu");
+var logotypeContainer = document.querySelector(".logotype-container");
 menuOpenButton.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.add("menu--show");
+  logotypeContainer.classList.add("menu--hide");
 });
 menuCloseButton.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.remove("menu--show");
+  logotypeContainer.classList.remove("menu--hide");
 });
