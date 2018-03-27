af225008af76d9b662e2bab75fca4faaad3e519d
Mike Kolotovich <k2-miha@mail.ru>
2018-03-28 00:52:33 +0300

    Третье задание

---
+++ Код и магия/js/setup.js
@@ -0,0 +1,47 @@
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = [' да Марья', ' Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var generator = function (array) {
+  return array[Math.random().toFixed(0)];
+}
+
+var fillBlock = function (array) {
+  for (var i = 0; i < 4; i++) {
+    array[i] = {
+      name: generator(WIZARD_NAMES) + generator(WIZARD_SURNAMES),
+      coatColor: generator(COAT_COLORS),
+      eyesColor: generator(EYES_COLORS)
+    }
+  }
+  return array;
+}
+
+var wizards = [];
+
+fillBlock(wizards);
+
+ var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
